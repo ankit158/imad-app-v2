9825898c587ddcecd7cf41774b80f2241d7724b1
ankit158 <ankitsingh5815148@gmail.com>
2017-06-14 06:41:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function() {
     
  };
  //make the request
- request.open('GET','http://ankit158.imad.hasura-app.io',true);
+ request.open('GET','http://ankit158.imad.hasura-app.io/counter',true);
  request.send(null);
 };
