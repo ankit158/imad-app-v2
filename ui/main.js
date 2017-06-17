b4009c18b039d31352d1b1487968c72841a977cf
ankit158 <ankitsingh5815148@gmail.com>
2017-06-18 00:47:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,26 +3,27 @@
 var button = document.getElementById("counter");
 
 
-
+counter = 0;
 button.onclick = function(){
+    
+counter = counter+1;
 
  //create a request object
- var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
   
  //capture the response
- request.onreadystatechange = function(){
-    if(request.readystate === XMLHttpRequest.DONE){
+// request.onreadystatechange = function(){
+  //  if(request.readystate === XMLHttpRequest.DONE){
     //take action
-    if(request.status===500){
-        var counter = request.responseText;
+//if(request.status===500){
+     //   var counter = request.responseText;
         var span = document.getElementById("count");
         span.innerHTML = counter.toString();
-    }
-    } 
+    
+    
  
     //take no action.wait.
  };
  //make the request
- request.open('GET','http://ankit158.imad.hasura-app.io/counter',true);
- request.send(null);
-};
+// request.open('GET','http://ankit158.imad.hasura-app.io/counter',true);
+// request.send(null);
