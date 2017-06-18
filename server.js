9f539828f049319ca9e6f3b5214be74e24a9db49
ankit158 <ankitsingh5815148@gmail.com>
2017-06-18 07:25:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
@@ -13,12 +13,69 @@ app.get('/counter', function(req,res) {
     counter = counter+1;
     res.send(counter.toString());
 });
+var articles={
+    'article-one':{
+       title:"article-one-ankit158" ,
+       heading:"article-one",
+       date:"10 june 2017",
+       content:`<p>
+                 This is article one.Behold your breathe.
+                </p>
+                <p>
+                That's doing nice.
+                </p>`    
+       
+                  },
+    'article-two':{ 
+       title:"article-one-ankit158" ,
+       heading:"article-one",
+       date:"10 june 2017",
+       content:`<p>
+                 This is article one.Behold your breathe.
+                </p>
+                <p>
+                That's doing nice.
+                </p>`  
+                  }
+};
+function createtemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmltemplate = `
+    <html>
+    <head>
+        <title>
+        ${title}
+        </title>
+    </head>
+    <body>
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <hr>
+        <h3>
+        ${heading}  
+        </h3>
+        <div>
+        ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+    </body>
+    </html>`;
+    return htmltemplate;
 
+}
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one', function (req,res) { 
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req,res) { 
+    var articleName = req.params.articleName;
+    res.send(createtemplate(articles[articleName]));
 }); 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
