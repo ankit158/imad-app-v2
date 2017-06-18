2a3ff36117cff19421e3bf96e1535936c9e38640
ankit158 <ankitsingh5815148@gmail.com>
2017-06-18 13:30:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
     database: 'ankit158',
     host: 'db.imad.hasura-app.io',
     port:'5432',
-    password:db-ankit158-48045
+    password: 'db-ankit158-48045'
 };
 
 app.get('/', function(req, res) {
