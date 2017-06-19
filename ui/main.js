cf8b9688d31b40d90f11f21d78240606bd01b611
ankit158 <ankitsingh5815148@gmail.com>
2017-06-19 07:16:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,29 @@
-//counter code
-
-var button = document.getElementById("counter");
-
-
-counter = 0;
-button.onclick = function(){
+//Submit ysername/password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
     
-counter = counter+1;
-
- //create a request object
-// var request = new XMLHttpRequest();
-  
- //capture the response
-// request.onreadystatechange = function(){
-  //  if(request.readystate === XMLHttpRequest.DONE){
-    //take action
-//if(request.status===500){
-     //   var counter = request.responseText;
-        var span = document.getElementById("count");
-        span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                alert('logged in successfully');
+            } else if(request.status === 403){
+                alert('username/password is incorrect');
+            } else if(request.status === 500){
+                alert('something went wrong');
+            }
+        }
+    };
     
     
- 
-    //take no action.wait.
- };
- //make the request
-// request.open('GET','http://ankit158.imad.hasura-app.io/counter',true);
-// request.send(null);
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://ankit158.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
+};
+
+
